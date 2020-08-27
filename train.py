1197e50f7c5e8854fba8fdb300c121f844bbe498
abebe9849 <masamasa20001002@gmail.com>
2020-08-28 01:20:42 +0900

    過去コンテストの名前、開催日時を取得　予定ではないので　お試し　後何が必要なのか...

--- train.py
+++ train.py
@@ -8,6 +8,17 @@ import re
 r=requests.get("https://atcoder.jp/contests/")
 soup=bs(r.text,"lxml")
 #print(soup.body)
+
 content = soup.body.find('div',id='main-div').find('div',id='main-container').find('div',class_='row')
 content = content.find('div',class_='col-lg-9 col-md-8')
-print(content)
+recent_contests = content.find('div',id='contest-table-recent').find("div",class_ ="panel panel-default").find("tbody")
+recent_contests = recent_contests.find_all("tr")
+print(len(recent_contests))
+for i in recent_contests:
+    #print(i)
+    #print("ここで区切り")
+    date = i.find("td",class_ = "text-center").find("a").text
+    print(date)
+    name = i.find_all("td")[1].find("a").text
+    print(name)
+    break
