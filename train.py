c373d7eb9e721713229ebac1333d5adc2f3f5446
abebe9849 <masamasa20001002@gmail.com>
2020-11-06 14:18:52 +0900

    前回との差分を考慮して、新規コンテストのみを出力:        print("new!!!!",info_) 現時点での開催予定コンテストをpickleで保存。

--- train.py
+++ train.py
@@ -4,6 +4,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import re
+import os
 import urllib.parse
 import sys
 r=requests.get("https://atcoder.jp/contests/")
@@ -23,6 +24,9 @@ upcoming_contests = upcoming_contests.find_all("tr")
 url_root  = "https://atcoder.jp"
 import urllib.parse
 
+
+
+
 def get_contest_info(upcoming_contests):#soupの一部を渡すと、(date,name,link)のlistを返す。
     infos =[]
     for i in upcoming_contests:
@@ -35,24 +39,29 @@ def get_contest_info(upcoming_contests):#soupの一部を渡すと、(date,name,
         #print(link)
         infos.append((date,name,link))
     return infos
-
+##これは前回との差分を考慮していないもの
 for info in get_contest_info(upcoming_contests):
     print(info)
 
-recent_contests = content.find('div',id='contest-table-recent').find("div",class_ ="panel panel-default").find("tbody")
-recent_contests = recent_contests.find_all("tr")
-print(len(recent_contests))
-for i in recent_contests:
-    #print(i)
-    #print("ここで区切り")
-    date = i.find("td",class_ = "text-center").find("a").text
-    print(date)
-    print("~~~~~~~~~~~~~~~~~~~~~~~")
-    name = i.find_all("td")[1].find("a").text
-    print(name)
-    link = i.find_all("td")[1].find("a").get("href")
-    link = urllib.parse.urljoin(url_root,link)
-    print("~~~~~~~~~~~~~~~~~~~~~~~")
+import pickle
+##前回の実行時の予定コンテスト情報をpickleで保存
+#前回差分fileがあれば読み込み
+##前回との差分をとる
+if os.path.isfile('diff_info.pickle'):
+    with open('diff_info.pickle', 'rb') as f:
+        diff_info = pickle.load(f)
+    upcoming_contests_info = list(set(get_contest_info(upcoming_contests))-set(diff_info))
+    if len(upcoming_contests_info)==0:
+        print("前回から新規に生えたコンテストはありません")
+    for info_ in upcoming_contests_info:
+        print("new!!!!",info_)
 
-    print(link)
-    break
+##現時点での開催予定コンテストを保存
+
+with open('diff_info.pickle', 'wb') as f:
+    pickle.dump(get_contest_info(upcoming_contests), f)
+
+
+
+
+
